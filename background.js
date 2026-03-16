7c8cc69d67482b81476c2c1de8e3953200a0bfa0
PK-1304 <pushkark1304@gmail.com>
2026-03-16 10:04:30 +0530

    minor

--- background.js
+++ background.js
@@ -3,23 +3,34 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         return
     }
 
-    const prompt = `
-    question_title: ${message.qsnTitle},
-    question_description: ${message.qsnDesc},
-    question_code_language: ${message.codeLanguage},
-    user_code = ${message.code}
-    `
+    (async () => {
+        try {
+            const prompt = `
+                question_title: ${message.qsnTitle},
+                question_description: ${message.qsnDesc},
+                question_code_language: ${message.codeLanguage},
+                user_code = ${message.code}
+            `;
 
-    fetch('https://api.pushkark1304.workers.dev', {
-        method: "POST",
-        headers: {
-            'Content-Type': "application/json"
-        },
-        body: JSON.stringify({ prompt })
-    })
-        .then(res => res.json())
-        .then(data => sendResponse({ data }))
-        .catch(err => sendResponse({ error: err.message }))
+            const res = await fetch('https://api.pushkark1304.workers.dev', {
+                method: "POST",
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify({ prompt })
+            });
 
-    return true
+            if (!res.ok) {
+                throw new Error(`Server responded with ${res.status}`);
+            }
+
+            const data = await res.json();
+            sendResponse({ data: data.data });
+        } catch (err) {
+            console.error("Fetch error:", err);
+            sendResponse({ error: err.message });
+        }
+    })();
+
+    return true;
 })
